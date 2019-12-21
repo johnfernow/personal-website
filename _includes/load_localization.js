ce78ad86bbf0742784293fe5824c474c507bc03d
John Fernow <31441141+johnfernow@users.noreply.github.com>
2019-12-21 02:42:01 -0500

    add language switcher

--- _includes/load_localization.js
+++ _includes/load_localization.js
@@ -1,6 +1,14 @@
 function loadLocalization(page) {
-  $(function(){
+  // autodetect language if user has not manually set it
+  if (localStorage.getItem("userLanguage") === null) {
     var opts = { pathPrefix: "{{site.baseurl}}/localizations" };
+  }
+  // load saved language preference if set
+  else {
+    var opts = { language: localStorage.userLanguage, pathPrefix: "{{site.baseurl}}/localizations" };
+  }
+
+  $(function(){
     $("[data-localize]").localize(page, opts);
   })
 }
